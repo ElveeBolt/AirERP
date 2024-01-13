1282a018af311e7457baae80bc580210b2c26b74
ElveeBolt <boltelvee@gmail.com>
2024-01-13 18:51:36 +0200

    Add webpack builder

---
+++ airerp/static/js/index.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+const flowbite = require('./flowbite.min')
+const dataTables = require('./jquery.dataTables.min')
+const mapbox = require('./mapbox-gl.js')
+const common = require('./common')
+
+import "../css/index.css";
