8b758d33bfa38d602d2064c6fbe19ff27626c5a0
ElveeBolt <boltelvee@gmail.com>
2024-01-19 12:13:45 +0200

    Fix bug with env SECRET_KEY

--- airerp/airerp/settings.py
+++ airerp/airerp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DEBUG', 'django-insecure-@=v0udi!h-g%f(pt^6z^(*6s4c+le44%w^*mgi%psm=r)wmn*#')
+SECRET_KEY = env('SECRET_KEY', 'django-insecure-@=v0udi!h-g%f(pt^6z^(*6s4c+le44%w^*mgi%psm=r)wmn*#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', True)
